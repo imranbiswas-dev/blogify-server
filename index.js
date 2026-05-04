1b7dd603f5a2b1587bc00498636ae02f2c38cab5
Imran <imranbiswas9299@gmail.com>
2026-05-04 16:33:36 +0600

    fixed all error

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-// MongoDB
+// Mongo URI
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@learning-db.ch9k7k3.mongodb.net/blogify?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri);
@@ -18,7 +18,7 @@ let blogCollection;
 let userCollection;
 let bookmarkCollection;
 
-// ===== DB CONNECT FIRST =====
+// ===== CONNECT DB =====
 async function connectDB() {
   await client.connect();
 
@@ -33,14 +33,17 @@ async function connectDB() {
 
 // ===== ROUTES =====
 
+// ROOT
 app.get("/", (req, res) => {
   res.send("Blogify Server Running 🚀");
 });
 
+// TEST
 app.get("/test", (req, res) => {
   res.send("API WORKING");
 });
 
+// ===== BLOG =====
 app.get("/blogs", async (req, res) => {
   const result = await blogCollection.find().toArray();
   res.send(result);
@@ -51,17 +54,63 @@ app.post("/blogs", async (req, res) => {
   res.send(result);
 });
 
-// ===== START SERVER AFTER DB =====
-async function startServer() {
-  try {
-    await connectDB();
+app.get("/blogs/:id", async (req, res) => {
+  const result = await blogCollection.findOne({
+    _id: new ObjectId(req.params.id),
+  });
+  res.send(result);
+});
 
-    app.listen(port, () => {
-      console.log("Server running on port", port);
-    });
-  } catch (error) {
-    console.log("Startup Error:", error);
-  }
+app.put("/blogs/:id", async (req, res) => {
+  const result = await blogCollection.updateOne(
+    { _id: new ObjectId(req.params.id) },
+    { $set: req.body },
+    { upsert: true }
+  );
+  res.send(result);
+});
+
+app.delete("/blogs/:id", async (req, res) => {
+  const result = await blogCollection.deleteOne({
+    _id: new ObjectId(req.params.id),
+  });
+  res.send(result);
+});
+
+// ===== USER =====
+app.post("/users", async (req, res) => {
+  const result = await userCollection.insertOne(req.body);
+  res.send(result);
+});
+
+// ===== BOOKMARK =====
+app.get("/bookmarks", async (req, res) => {
+  const email = req.query.userEmail;
+  if (!email) return res.send([]);
+
+  const result = await bookmarkCollection.find({ userEmail: email }).toArray();
+  res.send(result);
+});
+
+app.post("/bookmarks", async (req, res) => {
+  const result = await bookmarkCollection.insertOne(req.body);
+  res.send(result);
+});
+
+app.delete("/bookmarks/:id", async (req, res) => {
+  const result = await bookmarkCollection.deleteOne({
+    _id: new ObjectId(req.params.id),
+  });
+  res.send(result);
+});
+
+// ===== START =====
+async function startServer() {
+  await connectDB();
+
+  app.listen(port, () => {
+    console.log("Server running on port", port);
+  });
 }
 
 startServer();
